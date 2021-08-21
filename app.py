f4df57e65a1cc89d4b39d8fc481d8382af9828f7
Nikhil Dwa <nikhildwa627@gmail.com>
2021-08-21 22:41:33 +0545

    similar question printed

---
+++ app.py
@@ -0,0 +1,13 @@
+from src.main import *
+from src.clean_text import clean_text
+
+df = load_csv()
+text = input("Enter your Python related question: ")
+
+clean_sentence = clean_text(text)
+all_similarity_scores = finding_cosine_scores(df, clean_sentence)
+questions_index = top_five_questions_index(all_similarity_scores)
+
+print("\nThe top 5 questions similar to the input question are :\n")
+for count, index in enumerate(questions_index):
+    print(count + 1, "\t", df['Title'][index])
