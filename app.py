9f2603920bee88a691abc74c682725b935211621
Nikhil Dwa <nikhildwa627@gmail.com>
2021-08-23 08:13:33 +0545

    output updated

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from src import clean_text, load_csv, finding_cosine_scores, top_five_questions_index
 
+# Load the csv file in the dataframe
 df = load_csv()
+
+# Take the user input
 text = input("Enter your Python related question: ")
 
+# Preprocess the text, and calculate similarity score between the texts
 clean_sentence = clean_text(text)
 all_similarity_scores = finding_cosine_scores(df, clean_sentence)
-questions_index = top_five_questions_index(all_similarity_scores)
 
+# Find the top five question index and output top 5 similar questions
+questions_index = top_five_questions_index(all_similarity_scores)
 print("\nThe top 5 questions similar to the input question are :\n")
 for count, index in enumerate(questions_index):
     print(count + 1, "\t", df['Title'][index])
